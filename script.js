86ac4f2ea22b846c18b293a401c22d8c34b7682d
fatih kgokmen <fatih.kgokmen@gmail.com>
2021-02-24 13:58:35 -0500

    hold button updates

--- script.js
+++ script.js
@@ -38,6 +38,15 @@ btnRoll.addEventListener("click", function () {
     activePlayer = activePlayer === 0 ? 1 : 0;
     player0El.classList.toggle("player--active");
     player1El.classList.toggle("player--active");
-
   }
+});
+
+btnHold.addEventListener("click", function () {
+  scores[activePlayer] += currentScore;
+
+  document.getElementById(`current--${activePlayer}`).textContent =
+    scores[activePlayer];
+
+
+    
 });
